fa436d04a47a01886086a034a3f1cb4c1a9508a8
Epidemra <sergeinevgen2588@gmail.com>
2019-04-19 10:52:08 +0300

    Ellipse creator

---
+++ Paint/Paint/EllipseCreator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Paint
+{
+    class EllipseCreator : Creator
+    {
+
+        private static EllipseCreator instance;
+
+        private EllipseCreator()
+        { }
+
+        public static EllipseCreator getInstance()
+        {
+            if (instance == null)
+                instance = new EllipseCreator();
+            return instance;
+        }
+
+        public override Figure Create()
+        {
+            return new Ellipse();
+        }
+    }
+}
